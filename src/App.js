b73d92534da84390767f559444993f179cf3d2a2
Ayushya Amitabh <ayushyamitabh@gmail.com>
2017-06-17 11:58:32 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import profile from './res/profile.jpg';
-import {Card, CardHeader, CardMedia, CardTitle, CardText, CardActions, FlatButton} from 'material-ui';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <Card className="main-card">
-          <CardMedia>
-            <img src={profile} alt="" />
-          </CardMedia>
-          <CardTitle title="Ayushya Amitabh" className="card-title"/>
-          <CardText className="card-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-          <CardActions>
-            <FlatButton label="Action1" />
-            <FlatButton label="Action2" />
-          </CardActions>
-        </Card>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<MuiThemeProvider><App /></MuiThemeProvider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-injectTapEventPlugin();
