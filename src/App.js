ad840108af08cdd7e25a83c1427bcf994ceff302
Ayushya Amitabh <ayushyamitabh@gmail.com>
2017-06-24 14:21:23 -0400

    Fixed LineDot component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import LineDot from './LineDot.js';
-import {Card, CardTitle, CardText, Paper} from 'material-ui';
+import {Paper} from 'material-ui';
 import './App.css';
+import $ from 'jquery';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state={
-      events: [{alignment:'right',title:'JULY 17',subtitle:'0th Birthday',text:'Test text'}]
+      ninetySeven: [{alignment:'right',title:'JULY 17',subtitle:'0th Birthday',text:'Born in Rome, Italy - to the best parents.'}],
+      ninetyEight: [{alignment:'left',title:'AUGUST',subtitle:'First Flight',text:'Going back to the motherland - (Patna, Bihar) India - for the first time.'}]
     }
+  }
+  componentDidMount(){
+    $('body').animate({scrollTop:0},500);
   }
   render() {
     return (
@@ -17,9 +22,16 @@ class App extends Component {
           <h1>AYUSHYA AMITABH</h1>
           <h3>Start your travel down my memory lane</h3>
         </Paper>
+        <div className="down-line"></div>
         <LineDot
+          id = "ninetySeven"
           year = {1997}
-          events = {this.state.events}
+          events = {this.state.ninetySeven}
+        />
+        <LineDot
+          id = "ninetyEight"
+          year = {1998}
+          events = {this.state.ninetyEight}
         />
       </div>
     );
--- src/LineDot.js
+++ src/LineDot.js
@@ -9,23 +9,19 @@ class LineDot extends Component {
     }
   }
   componentDidMount() {
-    console.log(this.props.events);
     this.setState({
       events : this.props.events
     })
   }
   render() {
     return(
-      <div>
-        <div className="down-line"></div>
-        <div className="down-line"></div>
+      <div id={this.props.id}>
         <div className="line-dot"><h2>{this.props.year}</h2></div>
         {
           this.state.events.map((item, index)=>{
-            console.log(item);
-            console.log(item.alignment);
             return (
-              <LineDot
+              <LifeEvent
+                key={index}
                 alignment = {item.alignment}
                 title = {item.title}
                 subtitle = {item.subtitle}
@@ -43,12 +39,14 @@ function LifeEvent(props) {
   return (
     <div>
       <div className="down-line-small"></div>
-      <Card className={props.alignment === 'left' ? 'event-card-left' : 'event-card-right' }>
+      <div className={props.alignment === 'left' ? 'line-branch-left' : 'line-branch-right' }></div>
+      <Card zDepth={0} className={props.alignment === 'left' ? 'event-card-left' : 'event-card-right' }>
         <CardTitle title={props.title} subtitle={props.subtitle} />
         <CardText>
           {props.text}
         </CardText>
       </Card>
+      <div className="down-line-small"></div>
     </div>
   );
 }
