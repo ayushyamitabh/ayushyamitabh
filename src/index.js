e3ec592b7b10651120171a6f230ec159e83890c7
Ayushya Amitabh <ayushyamitabh@gmail.com>
2018-02-10 23:35:53 -0500

    Added data to firebase, adding projects section

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ import Projects from './redesign/Projects';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyCAZS-14yDcR8its35FkTa-qASjjC6cy-8",
+    authDomain: "ayushyamitabh-3c202.firebaseapp.com",
+    databaseURL: "https://ayushyamitabh-3c202.firebaseio.com",
+    projectId: "ayushyamitabh-3c202",
+    storageBucket: "ayushyamitabh-3c202.appspot.com",
+    messagingSenderId: "423859419654"
+  };
+firebase.initializeApp(config);
 
 injectTapEventPlugin();
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/redesign/Projects.js
+++ src/redesign/Projects.js
@@ -1,21 +1,51 @@
 import React, {Component} from 'react';
+import * as firebase from 'firebase';
 import './Projects.css';
 
 export default class Projects extends Component{
     constructor(props){
         super(props);
         this.state = {
-            my_projects: []
+            my_projects: null
         }
+    }
+
+    componentDidMount(){
+        firebase.database().ref('/Projects/').on('value', (snap)=>{
+            this.setState({
+                my_projects: snap.val()
+            })
+        })
     }
 
     render() {
         return (
             <div className="page">
-                asdf<br />
-                fdfas
-                asdfasdf
-                asdfasf
+                <div className="left-pane">
+                    <div className="card">
+                        <h1 className="section">Projects</h1>
+                    </div>
+                </div>
+                <div className="right-pane">
+                {
+                    this.state.my_projects === null?
+                    <div className="card error">
+                        There was a problem fetching data
+                        <br />
+                        :(
+                    </div>:
+                    Object.keys(this.state.my_projects).map((val,ind)=>{
+                        return (
+                            <div className="card">
+                                <h1 className="title">{this.state.my_projects[val].name}</h1>
+                                <h3 className="for">{this.state.my_projects[val].for}</h3>
+                                <p className="desc">{this.state.my_projects[val].desc}</p>
+                            </div>
+                        )
+                    })
+                }
+
+                </div>
             </div>
         );
     }
